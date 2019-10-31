5a86d3561a27649d7f00fcd039e79e8e7ee4aeb5
DaleCS <Dale.Seen@gmail.com>
2019-10-31 12:48:51 -0700

    Finished story

--- client/src/components/registration/Registration.js
+++ client/src/components/registration/Registration.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/registrationActions";
 
 import RegisterCard from "./RegisterCard";
 
@@ -97,17 +99,35 @@ class Registration extends Component {
     if (this.state.passwordField.length > 0) {
       if (this.state.passwordField.length >= 8) {
         const passRegex1 = new RegExp("[a-z]{5,}"); // Check if at least 5 lowercase letters
-        const passRegex2 = new RegExp("[A-Z]{1,}"); // Check if at least 1 uppercase letter
-        const passRegex3 = new RegExp("[0-9]{1,}"); // Check if at least 1 numerical character
-        const passRegex4 = new RegExp("^(.*)$"); // Check if one word
-        if (
-          passRegex1.test(this.state.passwordField) === true &&
-          passRegex2.test(this.state.passwordField) === true &&
-          passRegex3.test(this.state.passwordField) === true &&
-          passRegex4.test(this.state.passwordField) === true
-        ) {
+        if (passRegex1.test(this.state.passwordField) === true) {
+          const passRegex2 = new RegExp("[A-Z]{1,}"); // Check if at least 1 uppercase letter
+          if (passRegex2.test(this.state.passwordField) === true) {
+            const passRegex3 = new RegExp("[0-9]{1,}"); // Check if at least 1 numerical character
+            if (passRegex3.test(this.state.passwordField) === true) {
+              const passRegex4 = new RegExp(
+                "([!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~])"
+              ); // Check if at least 1 special character
+              if (passRegex4.test(this.state.passwordField) === true) {
+                const passRegex5 = new RegExp("^(.*)$"); // Check if one word
+                if (passRegex5.test(this.state.passwordField) === true) {
+                } else {
+                  validationResults.passwordField = "Password must be 1 word.";
+                }
+              } else {
+                validationResults.passwordField =
+                  "At least 1 special char needed.";
+              }
+            } else {
+              validationResults.passwordField =
+                "At least 1 numerical char neded.";
+            }
+          } else {
+            validationResults.passwordField =
+              "At least 1 uppercase char needed.";
+          }
         } else {
-          validationResults.passwordField = "Passwords is too weak.";
+          validationResults.passwordField =
+            "At least 5 lowercase chars needed.";
         }
       } else {
         validationResults.passwordField = "Password is too short.";
@@ -125,12 +145,24 @@ class Registration extends Component {
       validationResults.passwordVerifyField = "Please verify your password.";
     }
 
+    if (
+      validationResults.emailField === "" &&
+      validationResults.usernameField === "" &&
+      validationResults.passwordField === "" &&
+      validationResults.passwordVerifyField === ""
+    ) {
+      const newUser = {
+        email: this.state.emailField,
+        username: this.state.usernameField,
+        password: this.state.passwordField
+      };
+      this.props.registerUser(newUser, this.props.history);
+    }
+
     this.setState({
       ...this.state,
       inputValidation: validationResults
     });
-
-    // TODO: Call Backend for registration
   };
 
   render() {
@@ -162,4 +194,11 @@ class Registration extends Component {
   }
 }
 
-export default withStyles(styles)(Registration);
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withStyles(styles)(Registration));
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,7 @@
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+import errorReducer from "./errorReducer";
+
+export default combineReducers({
+  errors: errorReducer
+});
