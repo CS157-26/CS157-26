2d8340293cc4dcf4bc1b5ecbf278dae5fdcffaed
ZackFiner <zackary.finer@yahoo.com>
2019-11-08 18:27:58 -0800

    first commit

---
+++ sql/ticket_creation.sql
@@ -0,0 +1,3 @@
+USE pim;
+INSERT INTO tickets (item_id, author_id, title, content_text, current_status, priority, creation_date, modification_date, protected_status)
+VALUES (1, 1, "test_title_ticket", "stuf stufdaskl fdasjfk ldsajf kldsajfkldsa; fjdksalf ;jdsaklf ;jdsaklf ;das", "WIP", 1, CURRENT_TIME, CURRENT_TIME, false);
