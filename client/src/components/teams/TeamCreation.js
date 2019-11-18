7d8a1479b17586f9330ce564e45c54a447707a12
Antoine Ngu <antoine.ngu@sjsu.edu>
2019-11-17 20:00:49 -0800

    finish frontend

--- client/src/components/teams/TeamCreation.js
+++ client/src/components/teams/TeamCreation.js
@@ -1,12 +1,48 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-//import { registerUser } from "../../actions/registrationActions";
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Chip from '@material-ui/core/Chip';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import axios from "axios";
 
-import NewTeamCard from "./NewTeamCard.js";
 
-import { withStyles, Grid } from "@material-ui/core";
-
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: 2,
+    },
+    noLabel: {
+        marginTop: theme.spacing(3),
+    },
+    m2: {
+        marginTop: "1em"
+    },
+    caption: {
+        color: "#7f8c8d"
+    },
+    link: {
+        textDecoration: "none"
+    },
+    textField: {
+        width: "15em"
+    },
     pageSpacing: {
         paddingTop: "5em",
         paddingBottom: "5em"
@@ -22,94 +58,204 @@ const styles = theme => ({
     border: {
         border: "2px solid red"
     }
-});
+}));
 
-class TeamCreation extends Component {
-    constructor() {
-        super();
-        this.state = {
-            teamnameField: "",
-            assignedTypes: {},
-            inputValidation: {
-                teamnameField: "",
-                assignedTypes: ""
-            }
-        };
-    }
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
 
-    handleChange = e => {
-        e.preventDefault();
-        this.setState({
-            ...this.state,
-            [e.target.name]: e.target.value
-        });
-    };
+const types = [
+    { value: 1, name: 'Alpha' },
+    { value: 2, name: 'Bravo' },
+    { value: 3, name: 'Charlie' },
+    { value: 4, name: 'Delta' },
+    { value: 5, name: 'Echo' },
+    { value: 6, name: 'Foxtrot' },
+    { value: 7, name: 'Golf' },
+    { value: 8, name: 'Hotel' },
+    { value: 9, name: 'India' },
+    { value: 10, name: 'Juliet' },
+];
+const teams = ['abcdef', '123456', 'qwerty'];
 
-    validateInput = () => {
-        let validationResults = {
-            teamnameField: "",
-            assignedTypes: ""
-        };
-
-        if (this.state.teamnameField.length > 0) {
-            // check if name exists
-        } else {
-            validationResults.teamnameField = "Please enter a new team name.";
-        }
-
-        if (Object.keys(this.state.assignedTypes).length < 1) {
-            validationResults.usernameField = "Assigned types cannot be empty.";
-        }
-
-        if (
-            validationResults.teamnameField === "" &&
-            validationResults.assignedTypes === ""
-        ) {
-            const newTeam = {
-                name: this.state.teamnameField,
-                types: this.state.assignedTypes
-            };
-            console.log(newTeam);
-            //this.props.createTeam(newTeam, this.props.history);
-        }
-
-        this.setState({
-            ...this.state,
-            inputValidation: validationResults
-        });
-    };
-
-    render() {
-        const { classes } = this.props;
-
-        return (
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                align-items="center"
-                className={classes.pageSpacing}
-            >
-                <Grid item className={classes.cardSize}>
-                    <NewTeamCard
-                        teamnameField={this.state.teamnameField}
-                        assignedTypes={this.state.assignedTypes}
-                        handleChange={this.handleChange}
-                        inputValidation={this.state.inputValidation}
-                        validateInput={this.validateInput}
-                    />
-                </Grid>
-            </Grid>
-        );
-    }
+const fakeData = {
+    teams: teams,
+    types: types
 }
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
-});
+function getStyles(type, typeValue, theme) {
+    return {
+        fontWeight:
+            typeValue.indexOf(type) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    };
+}
 
-export default connect(
-    mapStateToProps,
-    // { registerUser }
-)(withStyles(styles)(TeamCreation));
+export default function TeamCreation() {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const [typeValue, setTypeValue] = React.useState([]);
+    const [teamName, setTeamName] = React.useState("");
+    const [teamNameError, setTeamNameError] = React.useState("");
+    const [existingData, setExistingData] = React.useState(fakeData);
+
+    React.useEffect(() => {
+        axios.get("/api/teams/data")
+            .then(res => setExistingData(res.data))
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+
+
+    const handleChange = event => {
+        setTypeValue(event.target.value);
+    };
+
+    const validateTeamName = value => {
+        let error = "";
+        if (value.length < 6) {
+            error = "You must enter a team name longer than 5 characters";
+        } else if (existingData.teams.indexOf(value) !== -1) {
+            error = "That team name already exists"
+        }
+        setTeamNameError(error);
+
+    }
+
+    const handleTextChange = event => {
+        setTeamName(event.target.value);
+        validateTeamName(event.target.value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const teamNameError = validateTeamName(teamName);
+        const numTypes = typeValue.length;
+        if (numTypes > 0 && teamNameError === "") {
+            const result = {
+                name: teamName,
+                types: typeValue.map(type => {
+                    return type.value;
+                })
+            }
+            console.log(result);
+        }
+    }
+
+    return (
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            align-items="center"
+            className={classes.pageSpacing}
+        >
+            <Grid item className={classes.cardSize}>
+                <Card className={classes.cardWidth}>
+                    <CardContent>
+                        <Grid
+                            container
+                            direction="column"
+                            justify="center"
+                            alignItems="center"
+                            spacing={2}
+                        >
+                            <Grid item>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justify="center"
+                                    alignItems="center"
+                                    spacing={2}
+                                >
+                                    <Grid item>
+                                        <Typography variant="h5">Create a new Team:</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Grid container direction="column" justify="center" spacing={1}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            name="teamNameField"
+                                            label="Team Name"
+                                            onChange={handleTextChange}
+                                            helperText={teamNameError}
+                                            error={teamNameError.length === 0 ? false : true}
+                                            className={classes.textField}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid container direction="row" alignItems="center">
+                                            <Grid item></Grid>
+                                            <FormControl className={classes.formControl}>
+                                                <InputLabel id="mutiple-chip-label">Types</InputLabel>
+                                                <Select
+                                                    labelid="mutiple-chip-label"
+                                                    id="mutiple-chip"
+                                                    multiple
+                                                    value={typeValue}
+                                                    onChange={handleChange}
+                                                    input={<Input id="select-multiple-chip" />}
+                                                    renderValue={selected => (
+                                                        <div className={classes.chips}>
+                                                            {selected.map(value => (
+                                                                <Chip key={value.value} label={value.name} className={classes.chip} />
+                                                            ))}
+                                                        </div>
+                                                    )}
+                                                    MenuProps={MenuProps}
+                                                >
+                                                    {existingData.types.map(type => (
+                                                        <MenuItem key={type.value} value={type} style={getStyles(type.name, typeValue, theme)}>
+                                                            {type.name}
+                                                        </MenuItem>
+                                                    ))}
+                                                </Select>
+                                            </FormControl>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item className={classes.m2}>
+                            <Grid
+                                container
+                                direction="row"
+                                justify="center"
+                                alignItems="center"
+                                spacing={2}
+                            >
+                                <Grid item>
+                                    <Button className={classes.backButton}>BACK</Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleSubmit}
+                                        disabled={(typeValue < 1) || (teamName.length < 5)}
+                                    >
+                                        CREATE TEAM
+                                </Button>
+                                </Grid>
+
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card >
+            </Grid>
+        </Grid >
+    );
+}
