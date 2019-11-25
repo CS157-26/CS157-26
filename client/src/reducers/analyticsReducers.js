0e679cbd76a2781fbc264a269937817b8ed3f7ca
ZackFiner <zackary.finer@yahoo.com>
2019-11-24 21:53:30 -0800

    Create analyticsReducers.js

---
+++ client/src/reducers/analyticsReducers.js
@@ -0,0 +1,47 @@
+import {
+    GET_ERRORS,
+    GET_ANALYTICS_OPEN_TICKETS,
+    GET_ANALYTICS_AVG_RESOLVE,
+    GET_ANALYTICS_TEAM_HISTORY
+} from "../actions/types";
+
+const initialState = {
+    open_tickets_data: {},
+    avg_resolve_data: {},
+    ticket_history: {},
+    errors: {}
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_ANALYTICS_OPEN_TICKETS: {
+            return {
+                ...state,
+                open_tickets_data: action.payload,
+                errors: {}
+            };
+        }
+        case GET_ANALYTICS_AVG_RESOLVE: {
+            return {
+                ...state,
+                avg_resolve_data: action.payload,
+                errors: {}
+            };
+        }
+        case GET_ANALYTICS_TEAM_HISTORY: {
+            return {
+                ...state,
+                ticket_history: action.payload,
+                errors: {}
+            };
+        }
+        case GET_ERRORS: {
+            return {
+                ...state,
+                errors: action.payload
+            };
+        }
+        default:
+            return state;
+    }
+}
