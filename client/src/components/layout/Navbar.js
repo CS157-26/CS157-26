8e91293210200bcf6b3b8e9c028bc09763b43ac2
DaleCS <Dale.Seen@gmail.com>
2019-11-29 15:42:49 -0800

    Added logout button and call to reducer

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { logoutUser } from "../../actions/loginActions";
 
 import { AppBar, Toolbar, Typography, Button, Grid } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
@@ -29,6 +32,49 @@ class Navbar extends Component {
   render() {
     const { classes } = this.props;
 
+    let loggedInMarkup;
+    if (this.props.auth && this.props.auth.isAuthenticated) {
+      loggedInMarkup = (
+        <>
+          <Grid item>
+            <Link to="/dashboard" className={classes.noStyling}>
+              <Button className={classes.btnClr}>
+                Dashboard
+              </Button>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link to="/login" className={classes.noStyling}>
+              <Button className={classes.btnClr} onClick={() => {
+                this.props.logoutUser();
+              }}>
+                Sign Out
+              </Button>
+            </Link>
+          </Grid>
+        </>
+      )
+    } else {
+      loggedInMarkup = (
+        <>
+          <Grid item>
+            <Link to="/registration" className={classes.noStyling}>
+              <Button className={classes.btnClr}>
+                Sign up
+              </Button>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link to="/login" className={classes.noStyling}>
+              <Button className={classes.btnClr}>
+                Sign in
+              </Button>
+            </Link>
+          </Grid>
+        </>
+      );
+    }
+
     return (
       <AppBar position="static">
         <Toolbar>
@@ -44,20 +90,7 @@ class Navbar extends Component {
             </Grid>
             <Grid item>
               <Grid container direction="row" spacing={2}>
-                <Grid item>
-                  <Link to="/registration" className={classes.noStyling}>
-                    <Button className={classes.btnClr}>
-                      Sign up
-                    </Button>
-                  </Link>
-                </Grid>
-                <Grid item>
-                <Link to="/login" className={classes.noStyling}>
-                    <Button className={classes.btnClr}>
-                      Sign in
-                    </Button>
-                  </Link>
-                </Grid>
+                {loggedInMarkup}
               </Grid>
             </Grid>
           </Grid>
@@ -67,4 +100,8 @@ class Navbar extends Component {
   }
 }
 
-export default withStyles(styles)(Navbar);
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {logoutUser})(withStyles(styles)(Navbar));
